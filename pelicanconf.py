88b1774b7261ea8286f966c1983ea19538e6bb9a
Yo Gis <hugoroussaffa@gmail.com>
2015-01-10 16:35:58 +1100

    config pure single theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,18 @@ PATH = 'content'
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'fr'
-THEME = "pure-single"
+#THEME = "pure-single"
+# uncomment for pure single theme
+#COVER_IMG_URL - Set the sidebar image.
+#PROFILE_IMG_URL - Set the image for the top circle cutout.
+#TAGLINE - Used for the page titles and some meta tags.
+#FAVICON_URL - Set the favicon image
+#DISQUS_SITENAME - Set this to enable disqus comments in articles.
+#DISQUS_ON_PAGES - Set this to True to enable disqus comments in pages.
+#GOOGLE_ANALYTICS - Set the Google Analytics code (eg. "UA-000000-00")
+#PIWIK_URL and PIWIK_SITE_ID - Set the URL and site-id for Piwik tracking. (Without 'http://')
+
+
 STATIC_PATHS = ['images']
 
 # Feed generation is usually not desired when developing
