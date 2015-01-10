4797ef648beae0e293797bf9b498b1ce6f96697f
Yo Gis <hugoroussaffa@gmail.com>
2015-01-10 21:01:45 +1100

    disable notebook

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,10 +13,10 @@ TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = u'fr'
 
 
-PLUGIN_PATHS = ['./pelican-plugins']
+PLUGIN_PATHS = ['../pelican-plugins/']
 PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
            'liquid_tags.youtube', 'liquid_tags.vimeo',
-           'liquid_tags.include_code', 'liquid_tags.notebook']
+           'liquid_tags.include_code',]
 
 LOAD_CONTENT_CACHE = False
 
