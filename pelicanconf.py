ec0df6860e0e48c58f98d1486ea5e606c67de29b
Yo Gis <hugoroussaffa@gmail.com>
2015-01-18 01:04:32 +1100

    home editing

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,13 +24,14 @@ THEME = "pure-single"
 # uncomment for pure single theme
 #COVER_IMG_URL - Set the sidebar image.
 PROFILE_IMG_URL = '/../../images/yogis_stamp_black_little.jpg'
-#TAGLINE - Used for the page titles and some meta tags.
+TAGLINE = geomatic, image and sound production
 #FAVICON_URL - Set the favicon image
 #DISQUS_SITENAME - Set this to enable disqus comments in articles.
 #DISQUS_ON_PAGES - Set this to True to enable disqus comments in pages.
 #GOOGLE_ANALYTICS - Set the Google Analytics code (eg. "UA-000000-00")
 #PIWIK_URL and PIWIK_SITE_ID - Set the URL and site-id for Piwik tracking. (Without 'http://')
-
+MENUITEMS = (('About Geomatic', 'pages/about.html'),
+         ('About IS Prod', 'pages/prod.html'),)
 
 STATIC_PATHS = ['images']
 
@@ -43,12 +44,11 @@ AUTHOR_FEED_RSS = None
 
 
 # Blogroll
-LINKS = (('YoGis Production', 'http://youtube.com/'),
-         ('YoGis Record', 'https://soundcloud.com/yogis-record'),)
+#LINKS = ((,),(,))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('souncloud', 'https://soundcloud.com/yogis-record'),
+          ('youtube', 'https://www.youtube.com/channel/UCK6L4K87OB9cQMdMZRnz5hg'),)
 
 DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
