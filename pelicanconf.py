e7ebfc307013455a57c9af3ec9e6f282ca126061
Yo Gis <hugoroussaffa@gmail.com>
2015-01-27 17:42:22 +1100

    change image

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,7 +22,7 @@ LOAD_CONTENT_CACHE = False
 
 THEME = "pure-single-yo"
 # uncomment for pure single theme
-#COVER_IMG_URL - Set the sidebar image.
+COVER_IMG_URL = '/../../images/taches.jpg'
 PROFILE_IMG_URL = '/../../images/yogis_stamp_black_little.jpg'
 TAGLINE = 'Geomatic, Image & Sound'
 #FAVICON_URL - Set the favicon image
