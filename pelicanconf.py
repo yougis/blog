591dfe2dc7133bdc3dba64b6e27d05b1c169d076
Yo Gis <hugoroussaffa@gmail.com>
2015-02-22 18:15:14 +0700

    add sitemap
    add plugin
    config sitemap

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,7 +16,8 @@ DEFAULT_LANG = u'fr'
 PLUGIN_PATHS = ['../pelican-plugins/']
 PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
            'liquid_tags.youtube', 'liquid_tags.vimeo',
-           'liquid_tags.include_code',]
+           'liquid_tags.include_code',
+           'pelican.plugins.sitemap',]
 
 LOAD_CONTENT_CACHE = False
 
@@ -42,6 +43,20 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# sitemap plugin config
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
 
 # Blogroll
 #LINKS = ((,),(,))
